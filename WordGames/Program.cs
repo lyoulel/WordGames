e81688bec520da93472b7df5be76f32be652c28a
Anon-K <50687833+Anon-K@users.noreply.github.com>
2022-01-09 14:34:00 +0800

    Hello Word Games!

--- WordGames/Program.cs
+++ WordGames/Program.cs
@@ -6,7 +6,7 @@ namespace WordGames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Word Games!");
         }
     }
 }
